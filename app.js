ee24a1646190ddf2a394069e516db8c78a965fd2
Haozhe Jiang <t@yesterday17.cn>
2017-07-28 23:01:23 +0800

    私人FM推流测试

--- app.js
+++ app.js
@@ -13,7 +13,12 @@ var rtmp = "rtmp://txy.live-send.acg.tv/live-txy/",
 var outputPath = rtmp + code;
 
 //var main_cookie = null;
-var main_cookie = `["__remember_me=true; Expires=Sat, 12 Aug 2017 14:37:09 GMT; Path=/; HttpOnly; hostOnly=true; aAge=2ms; cAge=5ms","MUSIC_U=ced7246c9b16c7788328d86b8fe5ae79eaadb63c6b5cfb97a9f2f26e059ef982801d544a8026e149ada05087280cb40efd8838aa19117b01; Expires=Sat, 12 Aug 2017 14:37:09 GMT; Path=/; HttpOnly; hostOnly=true; aAge=2ms; cAge=5ms","__csrf=7ca317c75a07441e81b4c541ccb6764e; Expires=Sat, 12 Aug 2017 14:37:19 GMT; Path=/; hostOnly=true; aAge=3ms; cAge=6ms"]`;
+var main_cookie = [
+    "MUSIC_U=ced7246c9b16c7788328d86b8fe5ae79c1ae184fff992d8efe699f9d8d4e0d7d2ec482ae31b90ad4cd67672ee0387699a8246a35c2da90bec3061cd18d77b7a0",
+    "__utma=1.389579652.1496909591.1496909591.1496909591.1",
+    "__csrf=6741eed22d326fa3c785abba0d16fa64",
+    "__utmz=1.1496909591.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)",
+    "__remember_me=true"];
 
 var sleep = function (time) {
     return new Promise(function (resolve, reject) {
@@ -120,11 +125,18 @@ var getPersonalFM = function (cookie) {
         var cookies = new Array();
         var j = request.jar()
 
+        for(let k in main_cookie){
+            j.setCookie(main_cookie[k], "http://localhost:3000/");
+        }
+
+        /*
         for (let k in cookie) {
             let ck = request.cookie(cookie[k].key + "=" + cookie[k].value);
             j.setCookie(ck, "http://localhost:3000/");
             //cookies.push(cookie[k].key + "=" + cookie[k].value);
         }
+        */
+        
         //var ck = request.cookie(cookies[2]);
         request({
             uri: "http://localhost:3000/personal_fm",
@@ -164,16 +176,28 @@ var getSongUrl = function (songid) {
 }
 
 var start = async function () {
+
+    //私人FM读取
     //防止高频ip访问禁止
-    let cookie = main_cookie == null ? await loginNetease() : JSON.parse(main_cookie);
-    let personalFM = await getPersonalFM(cookie);
-    console.log(JSON.stringify(personalFM));
+    let cookie = main_cookie,
+        personalFM = await getPersonalFM(cookie);
 
+    console.log(personalFM.code);
+    if(personalFM.code == 405){
+        cookie = await loginNetease();
+        main_cookie = JSON.stringify(main_cookie);
+        personalFM = await getPersonalFM(cookie);
+    }
+    for (var k in personalFM.data) {
+        let url = await getSongUrl(personalFM.data[k].id);
+        if (url != null)
+            await play(url);
+    }
 
-    //Play songlists
     /*
-    let playlist = await getPlaylist("766560163");
 
+    //歌单读取
+    let playlist = await getPlaylist("766560163");
     for (var k in playlist.privileges) {
         let url = await getSongUrl(playlist.privileges[k].id);
         //await downloadFile(url, "public/" + playlist.privileges[k].id + ".mp3");
@@ -181,14 +205,13 @@ var start = async function () {
         if (url != null)
             await play(url);
     }
-    */
 
-    //Play local files
-    /*
+    //本地歌曲读取
     for (var j = -1; j <= 4; j++) {
         await play("public/" + j + ".mp3");
         await sleep(1000);
     }
+
     */
 }
 
